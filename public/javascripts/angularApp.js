d91e93f5c3cede66e4ab91af14bdaef0c7c7666f
unknown <agarman@ENSHQ-MRKT03.ensi.local>
2015-03-24 15:00:22 -0400

    Front Page problems fixd

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -28,27 +28,51 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope,posts){
 app.controller('PostsCtrl', ['$scope', '$stateParams', 'posts', function($scope,$stateParams,posts){
   $scope.post = posts.posts[$stateParams.id];
   $scope.addComment = function(){
-    if($scope.body ===""){
-      return;
-    }
+    if($scope.body === '') { return; }
     $scope.post.comments.push({
       body: $scope.body,
-      author:'user',
-      upvotes: 0,
+      author: 'user',
+      upvotes: 0
     });
-    $scope.body ='';
+    $scope.body = '';
   };
 }])
 
-app.factory('posts',[ function(){
+app.factory('posts',['$http', function($http){
   var o = {
     posts: []
   };
-  return o;
-}]);
+  o.getAll = function() {
+    return $http.get('/posts').success(function(data){
+      angular.copy(data, o.posts);
+    });
+  };
 
-app.config('$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-  $stateProvider.state('home', {url: '/home',templateUrl: '/home.html', controller: 'MainCtrl'});
-  $stateProvider.state('posts',{url: '/posts/{id}', templateUrl: '/posts.html', controller: 'PostsCtrl'});
-  $urlRouterProvier.otherwise('home');
-})
+  return o;
+}
+
+]);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider){
+    $stateProvider.state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl',
+      resolve: {
+        postPromise: ['posts', function(posts){
+          return posts.getAll();
+        }]
+      }
+    });
+
+    $stateProvider.state('posts',{
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+
+    $urlRouterProvider.otherwise('home');
+}]);
